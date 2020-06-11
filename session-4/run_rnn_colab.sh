691d878456574a671894463a1b9d44f97ddf759a
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-06-11 19:09:30 +0700

    99.25% acc on test set

--- session-4/run_rnn_colab.sh
+++ session-4/run_rnn_colab.sh
@@ -1,3 +1,3 @@
-!python rnn.py --train_path '/content/gdrive/My Drive/MOUNT/dataset/train_encoded.txt'\
-                 --test_path '/content/gdrive/My Drive/MOUNT/dataset/test_encoded.txt' \
-                 --vocab_path '/content/gdrive/My Drive/MOUNT/dataset/vocab.txt'
+python rnn.py --train_path '/content/gdrive/My Drive/MOUNT/dataset/train_encoded.txt'\
+                --test_path '/content/gdrive/My Drive/MOUNT/dataset/test_encoded.txt' \
+                --vocab_path '/content/gdrive/My Drive/MOUNT/dataset/vocab.txt'
